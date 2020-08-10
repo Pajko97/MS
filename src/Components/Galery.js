788de9e185f046865a1be3de33f2e30d1560c72c
Pavle Janjusevic <devredhat@gmail.com>
2020-08-10 13:11:59 +0200

    new comm

--- src/Components/Galery.js
+++ src/Components/Galery.js
@@ -1,11 +1,58 @@
 import React, { Component } from 'react'
 import styles from '../css/Gallery.css'
+import magea from '../images/cont_img.jpg'
 
 class Galery extends Component {
     render() {
         return (
             <div className="gallery_wrapper">
-                <h1>Uskoro galerija..</h1>
+                <div className="gallery_div_left">
+                    <h2>Latest</h2>
+                    <div>
+                        <video src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" placeholder="http://via.placeholder.com/500x500" width={600} height={400} controls={true}></video>
+                        <p className="latest video_description">This video is made on 28/5 fishing on river Danube. We had to wait all day to catch some fish. Fish love our baits</p>
+                    </div>
+                </div>
+
+                <div className="right_wrapper">
+                    <div className="gallery_nav">
+                        <div className="video_link">Photos</div>
+                        <div className="photo_link">Videos</div>
+                    </div>
+                    <div className="gallery_div_right">
+
+                        <div className="video_container">
+                            <h3>Danube adventures with Moonshine</h3>
+                            <img src={magea}></img>
+                        </div>
+
+                        <div className="video_container">
+                            <h3>Danube adventures with Moonshine</h3>
+                            <img src={magea}></img>
+                        </div>
+
+                        <div className="video_container">
+                            <h3>Danube adventures with Moonshine</h3>
+                            <img src={magea}></img>
+                        </div>
+
+                        <div className="video_container">
+                            <h3>Danube adventures with Moonshine</h3>
+                            <img src={magea}></img>
+                        </div>
+
+                        <div className="video_container">
+                            <h3>Danube adventures with Moonshine</h3>
+                            <img src={magea}></img>
+                        </div>
+
+                        <div className="video_container">
+                            <h3>Danube adventures with Moonshine</h3>
+                            <img src={magea}></img>
+                        </div>
+
+                    </div>
+                </div>
             </div>
         )
     }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,8 +7,24 @@ class Home extends Component {
             <div class="home_wrapper">
                 <div class="home_main">
                     <img src={Logo}></img>
-                    <h1 id="sd">Mamciiiiiiiiaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</h1>
+                    <h1 id="sd">We are producing and shipping best quality baits for all kinds of fishing</h1>
                 </div>
+                <div className="animation_container">
+                    <figure class="circle">
+                        <div class="mask-a">
+                            <div class="bubble"></div>
+                            <div class="fish-green"></div>
+                            <div class="fish-red"></div>
+                            <div class="fish-yellow"></div>
+                            <div class="fishing">
+                                <div class="hook"></div>
+                                <div class="rope"></div>
+                            </div>
+                        </div>
+                    </figure>
+                </div>
+
+
             </div>
         )
     }
